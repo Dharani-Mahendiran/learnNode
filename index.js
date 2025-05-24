f842d3bf9370d4ed4c9c469849cfb91cccb5a575
dharani <dharani.mahendiran@ceiyone.com>
2025-05-24 21:49:46 +0530

    .env mongo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import {connectDB} from './config/db.js';
 import movieRoute from './routes/movies.route.js';
 import songRoutes from './routes/songs.route.js';
 
@@ -20,6 +21,8 @@ app.use('/songs', songRoutes);
 
 
 
-app.listen(PORT, ()=>{
+connectDB().then(() =>{
+    app.listen(PORT, ()=>{
      console.log(`Server running on Port http://localhost:${PORT}`)
-})
+    })
+});
